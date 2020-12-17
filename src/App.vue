b05865f445eeeb971db20d22be1b6d03ebcadf03
Florian Kimmel <flo.kimmel@gmail.com>
2020-12-17 17:58:02 +0100

    Display pagined users

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
-  <div id="app">
-  </div>
+	<div id="app">
+		<div class="container">
+			<UserList />
+		</div>
+	</div>
 </template>
 
 <script>
+import UserList from './components/UserList';
+
 export default {
-  name: 'App'
+	name: 'App',
+	components: {
+		UserList
+	},
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
