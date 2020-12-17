6316db2e73ee0c80cca777d4d2d2d2e66921caec
Florian Kimmel <flo.kimmel@gmail.com>
2020-12-17 17:34:30 +0100

    Created component UserThumbnail
    UserThumbnail displays the data of a user

---
+++ src/components/UserThumbnail.vue
@@ -0,0 +1,44 @@
+<template>
+	<b-card :title="name" :img-src="avatarURL" img-alt="Card image" img-left>
+		<b-card-text>
+			<b>E-mail:</b> {{ email }}
+			<br>
+			<b>ID:</b> {{ id }}
+		</b-card-text>
+	</b-card>
+</template>
+
+<script>
+import {UserModel} from '../models/UserModel';
+
+export default {
+	name: 'UserThumbnail',
+	props: {
+		userModel: {
+			type: UserModel,
+			default: function() {
+				return new UserModel(-1, 'example@mail.com', 'forname', 'lastname', 'avatar');
+			}
+		}
+	},
+	data: function() {
+		return {
+			id: this.userModel.getID(),
+			email: this.userModel.getEmail(),
+			firstName: this.userModel.getFirstName(),
+			lastName: this.userModel.getLastName(),
+			avatarURL: this.userModel.getAvatarURL()
+		}
+	},
+	computed: {
+		name: function() {
+			return this.firstName + ' ' + this.lastName;
+		}
+	}
+}
+</script>
+
+
+<style scoped>
+
+</style>
